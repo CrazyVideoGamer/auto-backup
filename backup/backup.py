f5c51e26e7a3e453cae1ff77d2b8e0bbbd151bdc
CrazyVideoGamer <crazyvideogamez.help@gmail.com>
2021-05-27 21:06:55 +0000

    fix the fix of the "cannot use default directory" bug

--- backup/backup.py
+++ backup/backup.py
@@ -27,7 +27,7 @@ args = create_parser().parse_args()
 
 if args.command == 'add':
   if args.directory == None and not default_directory_exists():
-  error_message('Cannot use default directory if default directory not set', 3)
+    error_message('Cannot use default directory if default directory not set', 3)
 
   target = args.target[0]
   directory = check_if_use_saved_directory(args.directory)
